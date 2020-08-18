b7be64835c3a2d299b876796635fa315cac0c60e
jurygr <59798153+jurygr@users.noreply.github.com>
2020-08-18 12:16:51 +0200

    Updates

---
+++ precache-manifest.3e0eeab199eae0b0148e56acaa48c3a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "688c851ab70c6e97a11efc7d3c13f49e",
+    "url": "/librarian/index.html"
+  },
+  {
+    "revision": "e52f0976bf5c6d60a4cb",
+    "url": "/librarian/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "0b3edde4ca41f3f83383",
+    "url": "/librarian/static/css/main.2c77e9b8.chunk.css"
+  },
+  {
+    "revision": "e52f0976bf5c6d60a4cb",
+    "url": "/librarian/static/js/2.6673b62e.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/librarian/static/js/2.6673b62e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b3edde4ca41f3f83383",
+    "url": "/librarian/static/js/main.642ea242.chunk.js"
+  },
+  {
+    "revision": "5b972b67f00c1afe63d8",
+    "url": "/librarian/static/js/runtime-main.a3a9b650.js"
+  },
+  {
+    "revision": "a6fa60826eaca6ff6ade1c0615d93ce2",
+    "url": "/librarian/static/media/book1.a6fa6082.jpg"
+  },
+  {
+    "revision": "57da1720c8ec3c5b7c19d6c99a227605",
+    "url": "/librarian/static/media/no-image-icon-23500.57da1720.jpg"
+  }
+]);
